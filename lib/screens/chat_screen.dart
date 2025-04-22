b2719907f12d9ec5092c3e57b5bf97237bb3a135
Cleiton Araujo <cleitonamc@gmail.com>
2025-04-21 21:22:00 -0300

    teste de comunicação firestoreDB

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -11,7 +14,21 @@ class ChatScreen extends StatelessWidget {
         itemBuilder: (ctx, i) => Container(
           padding: EdgeInsets.all(10),
           child: Text('Funcionou!'),
-        )),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chat')
+              .snapshots()
+              .listen((querySnapshot) {
+            querySnapshot.docs.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
