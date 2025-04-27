bcad2bd5e33e4b5427df3e6ac2c89ebcc25e52e5
Cleiton Araujo <cleitonamc@gmail.com>
2025-04-27 00:12:43 -0300

    Tratamento de erros do Auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,7 +2,10 @@
 
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/auth_error_messages.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -12,7 +15,34 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _handleSubmit(AuthData authData) {
+  final _fireBaseAuth = FirebaseAuth.instance;
+
+  Future<void> _handleSubmit(AuthData authData) async {
+    if (authData.email == null || authData.password == null) return;
+
+    try {
+      if (authData.isLogin) {
+        await _fireBaseAuth.signInWithEmailAndPassword(
+          email: authData.email!,
+          password: authData.password!,
+        );
+      } else {
+        await _fireBaseAuth.createUserWithEmailAndPassword(
+          email: authData.email!,
+          password: authData.password!,
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+
+      //verifica se o wodget ta montado para evitar erros
+      if (!mounted) return;
+
+      handleAuthError(context, e.code);
+    } catch (err) {
+      if (!mounted) return;
+      handleAuthError(context, 'undefined');
+    }
   }
 
   @override
---
+++ lib/utils/auth_error_messages.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void handleAuthError(BuildContext context, String error) {
+  // Mapa que associa o código de erro a uma mensagem
+  final errorMessages = {
+    'invalid-email': 'O e-mail fornecido não é válido.',
+    'user-disabled': 'A conta foi desativada.',
+    'user-not-found': 'Não encontramos uma conta com esse e-mail.',
+    'invalid-credential': 'Email ou Senha incorretos',
+    'wrong-password': 'Senha incorreta.',
+    'email-already-in-use': 'Este e-mail já está em uso.',
+    'weak-password': 'A senha deve ter no mínimo 6 caracteres.',
+    'operation-not-allowed': 'A operação não é permitida.',
+    'undefined': 'Ocorreu um erro inesperado.',
+  };
+
+  String errorMessage =
+      errorMessages[error] ?? 'Ocorreu um erro inesperado. Tente novamente.';
+
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(errorMessage),
+      backgroundColor: Theme.of(context).colorScheme.error,
+      duration: const Duration(seconds: 3),
+    ),
+  );
+}
